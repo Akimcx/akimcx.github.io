f26f647368ae06a57662e3a48bb165e1dd7269ed
Akim <silverdleo@protonmail.com>
2023-04-19 09:17:01 -0400

    Refactoring one 0 two

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const hamburger = document.getElementById("hamburger-checkbox");
-const sideBar = document.getElementById("sidebar");
+// const hamburger = document.getElementById("hamburger-checkbox");
+// const sideBar = document.getElementById("sidebar");
 
-hamburger.addEventListener("click", (e) => {
-  e.preventDefault();
-  sideBar.classList.toggle("show");
-});
+// hamburger.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   sideBar.classList.toggle("show");
+// });
+
+
+const switcher = document.getElementById("switcher")
+switcher.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark")
+})
